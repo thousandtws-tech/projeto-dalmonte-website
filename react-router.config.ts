b42ef34931cbb65a79a4e62338d46cc6c465732d
atlasengenharia-workspace <vinicius@thousandtws.com>
2026-07-24 14:23:01 -0300

    fix

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // SPA mode for 100% reliable, ultra-fast Vercel CDN deployment
+  ssr: false,
 } satisfies Config;
